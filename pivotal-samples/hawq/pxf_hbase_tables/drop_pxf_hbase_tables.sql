f350e11445a18e503774e9e8fe2bd176a42c7dca
Dave Roberts <droberts@gopivotal.com>
2015-04-22 17:34:51 -0500

    adding pivotal samples

---
+++ pivotal-samples/hawq/pxf_hbase_tables/drop_pxf_hbase_tables.sql
@@ -0,0 +1,17 @@
+DROP EXTERNAL TABLE retail_demo.categories_dim_hbase;
+
+DROP EXTERNAL TABLE retail_demo.customers_dim_hbase;
+
+DROP  EXTERNAL TABLE retail_demo.order_lineitems_hbase;
+
+DROP EXTERNAL TABLE retail_demo.orders_hbase;
+
+DROP EXTERNAL TABLE retail_demo.customer_addresses_dim_hbase;
+DROP EXTERNAL TABLE retail_demo.date_dim_hbase;
+
+
+DROP EXTERNAL TABLE retail_demo.email_addresses_dim_hbase;
+
+DROP EXTERNAL TABLE retail_demo.payment_methods_hbase;
+DROP EXTERNAL TABLE retail_demo.products_dim_hbase;
+
