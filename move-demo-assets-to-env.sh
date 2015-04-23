adfc3311b4e51ff342711195ee270cbf9d5cf3a3
Dave Roberts <droberts@gopivotal.com>
2015-04-23 10:23:40 -0500

    updating scripts

--- move-demo-assets-to-env.sh
+++ move-demo-assets-to-env.sh
@@ -3,10 +3,21 @@
 HOST=10.68.104.246
 USER=gpadmin
 
-echo downloading retail_demo dataset
-curl -O http://bds-demo.s3-website-us-east-1.amazonaws.com/retail_demo.zip > /dev/null
 
-echo downloading spring xd
-curl -O http://bds-demo.s3-website-us-east-1.amazonaws.com/spring-xd-1.0.2.RELEASE.zip > /dev/null
+if [ ! -f retail_demo.zip ]; then
+  echo retail_demo.zip file does not exist
+  echo downloading retail_demo.zip dataset
+  curl -O http://bds-demo.s3-website-us-east-1.amazonaws.com/retail_demo.zip > /dev/null
+else
+  echo retail_demo.zip already downloaded
+fi
 
-#scp -r retail_demo.zip spring-xd-1.0.2.RELEASE.zip setup-demo-env.sh hbase_path load-hbase.sh retail_demo pivotal-samples $USER@$HOST:~
+if [ ! -f spring-xd-1.0.2.RELEASE.zip ]; then
+  echo spring-xd-1.0.2.RELEASE.zip file does not exist
+  echo downloading spring-xd-1.0.2.RELEASE.zip
+  curl -O http://bds-demo.s3-website-us-east-1.amazonaws.com/spring-xd-1.0.2.RELEASE.zip > /dev/null
+else
+  echo spring-xd-1.0.2.RELEASE.zip already downloaded
+fi
+
+scp -r retail_demo.zip spring-xd-1.0.2.RELEASE.zip setup-demo-env.sh hbase_path load-hbase.sh retail_demo pivotal-samples $USER@$HOST:~
--- setup-demo-env.sh
+++ setup-demo-env.sh
@@ -3,6 +3,16 @@
 NAMENODE=is1-245.fe.gopivotal.com
 echo Setting up demo environment
 
+
+
+if [ ! -d "retail_demo" ]; then
+  unzip retail_demo.zip
+fi
+
+if [ ! -d "spring-xd-1.0.2.RELEASE" ]; then
+  unzip spring-xd-1.0.2.RELEASE.zip
+fi
+
 hadoop fs -rm -r -skipTrash /retail_demo
 hadoop fs -put retail_demo /
 
@@ -16,13 +26,16 @@ echo loading HAWQ tables
 pivotal-samples/hawq/hawq_tables/load_hawq_tables.sh
 
 echo verifying table counts
-pivotal-samples/hawq/hawq_tables/verify_load_hawq_tables.sh 
+pivotal-samples/hawq/hawq_tables/verify_load_hawq_tables.sh
 
 echo create external HDFS tables
 cat  pivotal-samples/hawq/pxf_tables/create_pxf_tables.sql | sed s/pivhdsne/$NAMENODE/ | psql
 
 echo drop existing hbase tables - ignore errors
-pivotal-samples/hawq/pxf_hbase_tables/drop_hbase_tables.pl 
+pivotal-samples/hawq/pxf_hbase_tables/load-hbase.sh
+
+echo load hbase tables
+pivotal-samples/hawq/pxf_hbase_tables/create_hbase_tables.pl
 
 echo create hbase tables
 pivotal-samples/hawq/pxf_hbase_tables/create_hbase_tables.pl
