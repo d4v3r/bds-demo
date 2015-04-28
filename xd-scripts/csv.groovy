262402e411f45d04bc93902576a5656264efb18b
d4v3r <ubiquitous_alias@yahoo.com>
2015-04-28 11:41:20 -0500

    Update csv.groovy

--- xd-scripts/csv.groovy
+++ xd-scripts/csv.groovy
@@ -2,18 +2,17 @@ import groovy.json.JsonSlurper
 
 def slurper = new JsonSlurper()
 def jsonPayload = slurper.parseText(payload)
-def var_id = jsonPayload?.id_str
-def var_fromUser = jsonPayload?.user?.screen_name
+def var_id = jsonPayload?.id
+def var_fromUser = jsonPayload?.from_user
 def var_text = jsonPayload?.text.replaceAll(/,/,' ').replaceAll(/'/,'').replaceAll(/"/,'')
 def var_createdAt = jsonPayload?.created_at
-def var_languageCode = jsonPayload?.lang
+def var_languageCode = jsonPayload?.language_code
 def var_retweetCount = jsonPayload?.retweet_count
 def var_retweet = jsonPayload?.retweeted
 def hashTags = jsonPayload?.entities?.hashtags
 
 
-def var_rand_id = java.util.UUID.randomUUID().toString()
-         
-def csv = var_rand_id + ',' + var_fromUser + ',' + var_createdAt + ',"' + var_text + '",' + var_languageCode + ',' + var_retweetCount
+var_retweetCount = var_retweetCount - 50000         
+def csv = var_id + ',' + var_fromUser + ',' + var_createdAt + ',"' + var_text + '",' + var_languageCode + ',' + var_retweetCount
 
 return csv
